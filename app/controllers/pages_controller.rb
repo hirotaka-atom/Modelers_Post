88d3528c2729162c2f2df2191e87fbcaa257bd41
hirotaka <hirotakamadomagi@icloud.com>
2021-05-12 17:43:49 +0900

    Timezone

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,9 @@ class PagesController < ApplicationController
     #@rankings = Post.includes(:bravo_users).where(created_at: Date.current)
     #@rankings = Bravo.includes(:post).where("created_at like ?", "%#{Date.current}%").sort {|a, b| (b.post.bravo_users.count <=> a.post.bravo_users.count).nonzero? || (b.post.impressions_count <=> a.post.impressions_count)}.take(10)
     #binding.pry
-    @rankings = Post.joins(:bravo_users).where("bravos.created_at like ?", "%#{Date.current}%").distinct.sort {|a, b| (b.bravo_users.count <=> a.bravo_users.count).nonzero? || (b.impressions_count <=> a.impressions_count)}.take(10)
+    #@rankings = Post.joins(:bravos).where("bravos.created_at like ?", "%#{Date.current}%").distinct.sort {|a, b| (b.bravo_users.count <=> a.bravo_users.count).nonzero? || (b.impressions_count <=> a.impressions_count)}.take(10)
+    @rankings = Post.joins(:bravos).where(bravos: {created_at: Date.current.in_time_zone.all_day}).distinct.sort {|a, b| (b.bravo_users.count <=> a.bravo_users.count).nonzero? || (b.impressions_count <=> a.impressions_count)}.take(10)
+    #@rankings = Bravo.left_joins(:post).where(created_at: Date.current.in_time_zone.all_day).sort {|a, b| (b.post.bravo_users.count <=> a.post.bravo_users.count).nonzero? || (b.post.impressions_count <=> a.post.impressions_count)}.take(10)
     @unlikes = Post.includes(:bravo_users).sort {|a, b| (a.bravo_users.count <=> b.bravo_users.count).nonzero? || (a.impressions_count <=> b.impressions_count)}.take(30)
   end
 
