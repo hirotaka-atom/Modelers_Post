72cd45508cec092e9499a3e58527b06cb2acda52
hirotaka <hirotakamadomagi@icloud.com>
2021-05-24 23:24:35 +0900

    fix_toppages_layout

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,18 +1,28 @@
 $(function(){
     $("#alert").fadeOut(5000);
-  });
+});
 
-  $(function() {
-      function readURL(input) {
-          if (input.files && input.files[0]) {
-          var reader = new FileReader();
-          reader.onload = function (e) {
-      $('#img_prev').attr('src', e.target.result);
-          }
-          reader.readAsDataURL(input.files[0]);
-          }
-      }
-      $("#img").change(function(){
-          readURL(this);
-      });
+$(function() {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+    $('#img_prev').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#img").change(function(){
+        readURL(this);
     });
+});
+
+$(function(){
+  $('#hamburger').on('click', function(){
+        $('.icon').toggleClass('close');
+        $('.sm').slideToggle();
+  });
+});
+
+$(function(){
+})
